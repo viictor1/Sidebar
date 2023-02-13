27034c5e724c0cf14c814433a2b221e25bffb4cd
Victor <victorgaspalmeida@hotmail.com>
2023-02-13 09:42:07 -0300

    bug fix

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const items = document.querySelectorAll('li span');
+const itemsArray = Array.from(document.querySelectorAll('li span'));
 const logo = document.querySelector('.sidebar img');
 const searchBar = document.querySelector('input');
 const menu = document.querySelector('.menu');
 const perfil = document.querySelector('.profileInfo');
-const user = document.querySelector('.user');
 const body = document.querySelector('body');
-const itemsArray = Array.from(items);
 
 menu.addEventListener('click', () => {
     itemsArray.forEach((item) => item.classList.toggle('hidden'))
     searchBar.classList.toggle('hidden');
     logo.classList.toggle('hidden');
     perfil.classList.toggle('hidden');
-    user.classList.toggle('userClosed')
     body.classList.toggle('closed')
 })
