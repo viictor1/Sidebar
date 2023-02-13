c93bf4bad19750b8e83583156377ec9ac9ba29c9
Victor <victorgaspalmeida@hotmail.com>
2023-02-13 15:12:04 -0300

    optimização

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const itemsArray = Array.from(document.querySelectorAll('li span'));
+const itemsArray = document.querySelectorAll('li span');
 const logo = document.querySelector('.sidebar img');
 const searchBar = document.querySelector('input');
 const menu = document.querySelector('.menu');
 const perfil = document.querySelector('.profileInfo');
-const body = document.querySelector('body');
 
 menu.addEventListener('click', () => {
     itemsArray.forEach((item) => item.classList.toggle('hidden'))
     searchBar.classList.toggle('hidden');
     logo.classList.toggle('hidden');
     perfil.classList.toggle('hidden');
-    body.classList.toggle('closed')
+    document.body.classList.toggle('closed')
 })
